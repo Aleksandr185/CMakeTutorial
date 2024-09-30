8a8fd50de57cae1aa37f4e5a980accce8a9fbcae
aleksandr@office.win11 <burlakin.alexandr@yandex.ru>
2024-09-30 09:18:47 +0300

    Exercise 2 - Specifying the C++ Standard

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include "iostream"
+#include <optional>
+
+std::optional<int> foo(int a)
+{
+  if (a > 0)
+    return std::optional<int>(a);
+
+  return std::optional<int>();
+}
 
 int main()
 {
-  std::cout << "Hello world! Exercise 1.";
+  std::cout << "Hello world! Exercise 1." << std::endl;
+  std::cout << foo(5).has_value();
   return 0;
 }
