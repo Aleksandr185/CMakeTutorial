6449d43bc7b8af28787cfa095b56977951f196b9
aleksandr@office.win11 <burlakin.alexandr@yandex.ru>
2024-09-30 10:24:14 +0300

    Exercise 3 - Adding a Version Number and Configured Header File

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "iostream"
 #include <optional>
 
+#include "version.h"
+
 std::optional<int> foo(int a)
 {
   if (a > 0)
@@ -11,7 +13,8 @@ std::optional<int> foo(int a)
 
 int main()
 {
-  std::cout << "Hello world! Exercise 1." << std::endl;
+  std::cout << "Hello world!" << std::endl;
+  std::cout << "Version is: " << APP_VERSION << std::endl;
   std::cout << foo(5).has_value();
   return 0;
 }
