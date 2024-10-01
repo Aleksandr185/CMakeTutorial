3b4e8525e3b8d0a7939080a9a274754e79c8b878
aleksandr@office.win11 <burlakin.alexandr@yandex.ru>
2024-10-01 14:05:08 +0300

    Step 2: Exercise 2 - Adding an Option

--- main.cpp
+++ main.cpp
@@ -18,6 +18,12 @@ int main()
   std::cout << "Version is: " << APP_VERSION << std::endl;
   std::cout << "has value: " << foo(5).has_value() << std::endl;
 
+#ifdef OPTION_ONE_DEF
+  std::cout << "OPTION_ONE_DEF is defined!" << std::endl;
+#else
+  std::cout << "OPTION_ONE_DEF is not defined!" << std::endl;
+#endif
+
   utils::print("test utils::print function.");
 
   return 0;
